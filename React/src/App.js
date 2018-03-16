461bf473e14a02bd0ab9f9db60e6a45d94bdb6b7
Homero Barbosa <hbarbosa@paylocity.com>
2018-03-15 17:58:56 -0700

    Componetize page

--- React/src/App.js
+++ React/src/App.js
@@ -1,23 +1,12 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css'
 
-class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
-
+class BookSearch extends React.Component{
   render() {
     return (
-      <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
+      <div className="search-books">
             <div className="search-books-bar">
               <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
               <div className="search-books-input-wrapper">
@@ -37,8 +26,14 @@ class BooksApp extends React.Component {
               <ol className="books-grid"></ol>
             </div>
           </div>
-        ) : (
-          <div className="list-books">
+    )
+  }
+}
+
+class BookShelf extends React.Component{
+  render() {
+    return (
+      <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
@@ -197,6 +192,28 @@ class BooksApp extends React.Component {
               <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
             </div>
           </div>
+    )
+  }
+}
+
+class BooksApp extends React.Component {
+  state = {
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+    showSearchPage: false
+  }
+
+  render() {
+    return (
+      <div className="app">
+        {this.state.showSearchPage ? (
+          <BookSearch/>
+        ) : (
+          <BookShelf/>
         )}
       </div>
     )
