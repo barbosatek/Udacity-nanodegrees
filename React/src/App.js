0736dcf5752cfdce23fd60547d3cd80411bbcb17
Homero Barbosa <hbarbosa@paylocity.com>
2018-03-18 13:48:47 -0700

    Merge pages

--- React/src/App.js
+++ React/src/App.js
@@ -1,14 +1,55 @@
 import React from 'react'
 import BookShelf from './BookShelf'
-import BookSearch from './BookSearch'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css'
+import * as BooksAPI from "./BooksAPI";
+import Book from "./Book";
 
-class BookList extends React.Component{
+class BooksApp extends React.Component {
   state = {
     currentlyReadingBooks: [],
     wantToReadBooks: [],
-    readBooks: []
+    readBooks: [],
+    books: [],
+    query: ""
+  }
+
+  mapBook(book) {
+    let mappedBook = {
+      imageUrl: book.imageLinks.thumbnail,
+      title: book.title,
+      authors: '',
+      id: book.id
+    };
+
+    if(book.authors){
+      mappedBook.authors = book.authors.join(", ")
+    }
+
+    return mappedBook;
+  }
+
+  updateQuery = query => {
+    BooksAPI.search(query.trim()).then(books => {
+      if (!Array.isArray(books)) {
+        return;
+      }
+
+      books = books.map(book => {
+        return this.mapBook(book);
+      });
+
+      this.setState({ books });
+    });
+  };
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      let mappedBooks = books.map(book => {
+        return this.mapBook(book);
+      });
+      this.setState({ books: mappedBooks });
+    });
   }
 
   moveBook(book, targetShelf){
@@ -31,7 +72,9 @@ class BookList extends React.Component{
 
   render() {
     return (
-      <div className="list-books">
+      <Router>
+        <div className="app">
+          <Route exact path="/" render={() => <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
@@ -45,28 +88,37 @@ class BookList extends React.Component{
             <div className="open-search">
               <Link to="/search">Add a book</Link>
             </div>
+          </div>} />
+          <Route exact path="/search" render={() => <div className="search-books">
+        <div className="search-books-bar">
+          <a
+            className="close-search"
+            onClick={() => this.setState({ showSearchPage: false })}
+          >
+            Close
+          </a>
+          <div className="search-books-input-wrapper">
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              onChange={event => this.updateQuery(event.target.value)}
+            />
           </div>
-    )
-  }
-}
-
-class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
-
-  render() {
-    return (
-      <Router>
-        <div className="app">
-          <Route exact path="/" component={BookList} />
-          <Route exact path="/search" component={BookSearch} />
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {this.state.books.map(book => (
+              <li key={book.id}>
+                <Book
+                  book={book}
+                  onMoveBook={(b, s) => this.moveBook(b, s)}
+                  shelfId="search"
+                />
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>} />
       </div>
       </Router>
     )
