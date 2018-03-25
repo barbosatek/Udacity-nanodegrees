16fee03859de3529d5076dbf8d3823d1b0303c0b
Homero Barbosa <hbarbosa@paylocity.com>
2018-03-24 18:59:37 -0700

    Format and clean up

--- Front-End Web Developer/Project 2/js/card.js
+++ Front-End Web Developer/Project 2/js/card.js
@@ -1,57 +1,57 @@
 // Encapsulates the Card object, its interaction with the DOM and state
-const Card = function(cardNode, value){
+const Card = function(cardNode, value) {
     cardNode.children[0].className = value;
-
+   
     return {
-        state: new CardState(value),
-        node: cardNode,
-        closeCard: function(){
-            cardNode.classList.remove("open");
-            cardNode.classList.remove("show");
-            this.state.isOpened = false;
-        },
-        open: function(){
-            cardNode.classList.add("open");
-            cardNode.classList.add("show");
-            this.state.isOpened = true;
-        },
-        match: function(){
-            cardNode.classList.add("match");
-            this.state.isMatched = true;
-        },
-        unmatch: function(){
-            cardNode.classList.remove("match");
-            this.state.isMatched = false;
-        },
-        isMatched: function(){
-            return cardNode.classList.contains("match");
-        },
-        isOpened: function(){
-            return cardNode.classList.contains("open");
-        },
-        getValue: function(){
-            return cardNode.children[0].className
-        },
-        setValue: function(value){
-            cardNode.children[0].className = value;
-            this.state.value = value;
-        },
-        restoreFromState: function(state){
-            if(state.isMatched){
-                this.match();
-            } else{
-                this.unmatch();
-            }
-
-            if(state.isOpened){
-                this.open();
-            }else{
-                this.closeCard();
-            }
-
-            this.state.id = state.id;
-            this.state.value = state.value;
-            cardNode.children[0].className = state.value;
-        }
+     state: new CardState(value),
+     node: cardNode,
+     closeCard: function() {
+      cardNode.classList.remove("open");
+      cardNode.classList.remove("show");
+      this.state.isOpened = false;
+     },
+     open: function() {
+      cardNode.classList.add("open");
+      cardNode.classList.add("show");
+      this.state.isOpened = true;
+     },
+     match: function() {
+      cardNode.classList.add("match");
+      this.state.isMatched = true;
+     },
+     unmatch: function() {
+      cardNode.classList.remove("match");
+      this.state.isMatched = false;
+     },
+     isMatched: function() {
+      return cardNode.classList.contains("match");
+     },
+     isOpened: function() {
+      return cardNode.classList.contains("open");
+     },
+     getValue: function() {
+      return cardNode.children[0].className
+     },
+     setValue: function(value) {
+      cardNode.children[0].className = value;
+      this.state.value = value;
+     },
+     restoreFromState: function(state) {
+      if (state.isMatched) {
+       this.match();
+      } else {
+       this.unmatch();
+      }
+   
+      if (state.isOpened) {
+       this.open();
+      } else {
+       this.closeCard();
+      }
+   
+      this.state.id = state.id;
+      this.state.value = state.value;
+      cardNode.children[0].className = state.value;
+     }
     }
-}
+   }
--- Front-End Web Developer/Project 2/js/game.js
+++ Front-End Web Developer/Project 2/js/game.js
@@ -2,251 +2,249 @@
 // Format / clean up
 
 // Encapsulates the Game object, its interactions with the DOM, its state and events.
-const Game = function(){
+const Game = function() {
     let state;
     let cardDeck;
     let isClosingCard = false;
-
+   
     // Initializes the game given an array of nodes
-    function initGame(){
-        let storedState;
-        try{
-            storedState = JSON.parse(localStorage.getItem('Game.State'));
-        } catch(e){
-            console.log('Unable to deserialize state. State will be reset')
-        }
-
-        if(!storedState){
-            resetGame();
-        } else{
-            restoreGame(storedState);
-        }
-
-        document.querySelector('.restart').addEventListener("click", (e) => {
-            resetGame();
-        }, false);
-
-        document.querySelector('.play-again').addEventListener("click", (e) => {
-            resetGame();
-        }, false);
-
-        let intervalID = window.setInterval((state, cardDeck) => {
-            handleTimeIntervar()
-        }, 1000, state, cardDeck);
-
-        handleTimeIntervar()
-        updateScreenMode(true);
+    function initGame() {
+     let storedState;
+     try {
+      storedState = JSON.parse(localStorage.getItem('Game.State'));
+     } catch (e) {
+      console.log('Unable to deserialize state. State will be reset')
+     }
+   
+     if (!storedState) {
+      resetGame();
+     } else {
+      restoreGame(storedState);
+     }
+   
+     document.querySelector('.restart').addEventListener("click", (e) => {
+      resetGame();
+     }, false);
+   
+     document.querySelector('.play-again').addEventListener("click", (e) => {
+      resetGame();
+     }, false);
+   
+     let intervalID = window.setInterval((state, cardDeck) => {
+      handleTimeIntervar()
+     }, 1000, state, cardDeck);
+   
+     handleTimeIntervar()
+     updateScreenMode(true);
     }
-
+   
     // Updates game deck or congratulations modal depending on the game state.
-    function updateScreenMode(scrollTop){
-        let modalNode = document.querySelector('.congrats-modal');
-        let gameNode = document.querySelector('.game');
-        let modalTextNode = document.querySelector('.congrats-body').children[0];
-
-        if(!state.isGameOver){
-            modalNode.style.visibility = 'hidden';
-            modalNode.style.display = 'none';
-
-            gameNode.style.visibility = 'visible';
-            gameNode.style.display = 'flex';
-        } else{
-            modalNode.style.visibility = 'visible';
-            modalNode.style.display = 'flex';
-            
-            gameNode.style.visibility = 'hidden';
-            gameNode.style.display = 'none';
-
-            modalTextNode.textContent = 'With ' + state.totalMoves + ' Moves and ' + calculateRemainingStars() + ' stars.';
-        }
-
-        if(scrollTop){
-            window.setTimeout(() => {
-                window.scrollTo(0, 0)
-            }, 300);
-        }
+    function updateScreenMode(scrollTop) {
+     let modalNode = document.querySelector('.congrats-modal');
+     let gameNode = document.querySelector('.game');
+     let modalTextNode = document.querySelector('.congrats-body').children[0];
+   
+     if (!state.isGameOver) {
+      modalNode.style.visibility = 'hidden';
+      modalNode.style.display = 'none';
+   
+      gameNode.style.visibility = 'visible';
+      gameNode.style.display = 'flex';
+     } else {
+      modalNode.style.visibility = 'visible';
+      modalNode.style.display = 'flex';
+   
+      gameNode.style.visibility = 'hidden';
+      gameNode.style.display = 'none';
+   
+      modalTextNode.textContent = 'With ' + state.totalMoves + ' Moves and ' + calculateRemainingStars() + ' stars.';
+     }
+   
+     if (scrollTop) {
+      window.setTimeout(() => {
+       window.scrollTo(0, 0)
+      }, 300);
+     }
     }
-
+   
     // Handles timier and displaying stars
-    function handleTimeIntervar(){
-        if(!state.isGameOver){
-            state.elapsedSeconds = state.elapsedSeconds + 1;
-            document.querySelector('.timer').textContent = 'Elapsed Time:' + state.elapsedSeconds + 's.';
-
-            let remainingStars = calculateRemainingStars();
-            displayStars(remainingStars);
-
-            saveState();
-        }
+    function handleTimeIntervar() {
+     if (!state.isGameOver) {
+      state.elapsedSeconds = state.elapsedSeconds + 1;
+      document.querySelector('.timer').textContent = 'Elapsed Time:' + state.elapsedSeconds + 's.';
+   
+      let remainingStars = calculateRemainingStars();
+      displayStars(remainingStars);
+   
+      saveState();
+     }
     }
-
+   
     // Returns the current stars given the elapsed time
-    function calculateRemainingStars(){
-        if(state.elapsedSeconds > 0 && state.elapsedSeconds < 20){
-            return 3;
-        }
-
-        if(state.elapsedSeconds >= 20 && state.elapsedSeconds < 40){
-            return 2;
-        }
-
-        if(state.elapsedSeconds >= 40 && state.elapsedSeconds < 60){
-            return 1;
-        }
-
-        if(state.elapsedSeconds > 60){
-            return 0;
-        }
-
-        throw 'Unable to calculate remaining stars';
+    function calculateRemainingStars() {
+     if (state.elapsedSeconds > 0 && state.elapsedSeconds < 20) {
+      return 3;
+     }
+   
+     if (state.elapsedSeconds >= 20 && state.elapsedSeconds < 40) {
+      return 2;
+     }
+   
+     if (state.elapsedSeconds >= 40 && state.elapsedSeconds < 60) {
+      return 1;
+     }
+   
+     if (state.elapsedSeconds > 60) {
+      return 0;
+     }
+   
+     throw 'Unable to calculate remaining stars';
     }
-
+   
     // Displays star score in the DOM 
-    function displayStars(count){
-        let starNodes = [...document.querySelectorAll('.fa-star')];
-        if(starNodes.length === 3){
-            if(count === 3){
-                starNodes[0].style.visibility = "";
-                starNodes[1].style.visibility = "";
-                starNodes[2].style.visibility = "";
-            } else if(count === 2){
-                starNodes[0].style.visibility = "";
-                starNodes[1].style.visibility = "";
-                starNodes[2].style.visibility = "hidden";
-            } else if(count === 1){
-                starNodes[0].style.visibility = "";
-                starNodes[1].style.visibility = "hidden";
-                starNodes[2].style.visibility = "hidden";
-            } else if(count === 0){
-                starNodes[0].style.visibility = "hidden";
-                starNodes[1].style.visibility = "hidden";
-                starNodes[2].style.visibility = "hidden";
-            }
-        }
+    function displayStars(count) {
+     let starNodes = [...document.querySelectorAll('.fa-star')];
+     if (starNodes.length === 3) {
+      if (count === 3) {
+       starNodes[0].style.visibility = "";
+       starNodes[1].style.visibility = "";
+       starNodes[2].style.visibility = "";
+      } else if (count === 2) {
+       starNodes[0].style.visibility = "";
+       starNodes[1].style.visibility = "";
+       starNodes[2].style.visibility = "hidden";
+      } else if (count === 1) {
+       starNodes[0].style.visibility = "";
+       starNodes[1].style.visibility = "hidden";
+       starNodes[2].style.visibility = "hidden";
+      } else if (count === 0) {
+       starNodes[0].style.visibility = "hidden";
+       starNodes[1].style.visibility = "hidden";
+       starNodes[2].style.visibility = "hidden";
+      }
+     }
     }
-
+   
     // Restores the game given the game state
-    function restoreGame(storedState){
-        document.querySelector('.moves').textContent = storedState.totalMoves;
-
-        cardDeck = new CarDeck();
-        cardDeck.restoreFromState(storedState.cardStates)
-        bindClickEvent();
-
-        state = {
-            matchingCard: storedState.matchingCardState !== null
-                ? cardDeck.cards.find(x => x.state.id === storedState.matchingCardState.id)
-                : null,
-            isMatching: storedState.isMatching,
-            elapsedSeconds: storedState.elapsedSeconds,
-            isGameOver: storedState.isGameOver,
-            totalMoves: storedState.totalMoves
-        }
+    function restoreGame(storedState) {
+     document.querySelector('.moves').textContent = storedState.totalMoves;
+   
+     cardDeck = new CarDeck();
+     cardDeck.restoreFromState(storedState.cardStates)
+     bindClickEvent();
+   
+     state = {
+      matchingCard: storedState.matchingCardState !== null ? cardDeck.cards.find(x => x.state.id === storedState.matchingCardState.id) : null,
+      isMatching: storedState.isMatching,
+      elapsedSeconds: storedState.elapsedSeconds,
+      isGameOver: storedState.isGameOver,
+      totalMoves: storedState.totalMoves
+     }
     }
-
+   
     // Resets the game to its initial value, shuffles the cards and clears the local storage
-    function resetGame(){
-        localStorage.setItem('Game.State', null);
-        document.querySelector('.moves').textContent = "0";
-
-        if(cardDeck){
-            cardDeck.clear();
-        }
-        
-        cardDeck = new CarDeck();
-        cardDeck.shuffle();
-        bindClickEvent();
-
-        cardDeck.cards.forEach(card => {
-            card.closeCard()
-            card.unmatch()
-        });
-
-        state = {
-            matchingCard: null,
-            isMatching: false,
-            totalMoves: 0,
-            elapsedSeconds: 0,
-            isGameOver: false
-        }
-
-        updateScreenMode(true)
+    function resetGame() {
+     localStorage.setItem('Game.State', null);
+     document.querySelector('.moves').textContent = "0";
+   
+     if (cardDeck) {
+      cardDeck.clear();
+     }
+   
+     cardDeck = new CarDeck();
+     cardDeck.shuffle();
+     bindClickEvent();
+   
+     cardDeck.cards.forEach(card => {
+      card.closeCard()
+      card.unmatch()
+     });
+   
+     state = {
+      matchingCard: null,
+      isMatching: false,
+      totalMoves: 0,
+      elapsedSeconds: 0,
+      isGameOver: false
+     }
+   
+     updateScreenMode(true)
     }
-
+   
     // Saves the current game state
-    function saveState(){
-        localStorage.setItem('Game.State', JSON.stringify({
-            matchingCardState: state.matchingCard !== null ? state.matchingCard.state : null,
-            isMatching: state.isMatching,
-            totalMoves: state.totalMoves,
-            cardStates: [...cardDeck.cards.map(x => x.state)],
-            elapsedSeconds: state.elapsedSeconds,
-            isGameOver: state.isGameOver
-        }));
+    function saveState() {
+     localStorage.setItem('Game.State', JSON.stringify({
+      matchingCardState: state.matchingCard !== null ? state.matchingCard.state : null,
+      isMatching: state.isMatching,
+      totalMoves: state.totalMoves,
+      cardStates: [...cardDeck.cards.map(x => x.state)],
+      elapsedSeconds: state.elapsedSeconds,
+      isGameOver: state.isGameOver
+     }));
     }
-
+   
     // Increases the move counter and displays it
-    function increaseMoveCounter(){
-        state.totalMoves++;
-        document.querySelector('.moves').textContent = state.totalMoves;
+    function increaseMoveCounter() {
+     state.totalMoves++;
+     document.querySelector('.moves').textContent = state.totalMoves;
     }
-
+   
     // Binds the click event on the cards and manages the main click logic and state that determines when 
     // the game was won
-    function bindClickEvent(){
-        cardDeck.cards.forEach(card => {
-            card.node.addEventListener("click", (e) => {
-                if(isClosingCard || card.isOpened() || card.isMatched()){
-                    return;
-                }
-
-                card.open();
-
-                if(state.isMatching){
-                    if(state.matchingCard.getValue() === card.getValue()){
-                        
-                        card.match()
-                        state.matchingCard.match();
-
-                        if(cardDeck.cards.filter(x => x.isMatched()).length === cardDeck.cards.length){
-                            state.isGameOver = true;
-                            saveState();
-                            updateScreenMode(true);
-                            return;
-                        }
-
-                        state.isMatching = false;
-                        state.matchingCard = null;
-                        increaseMoveCounter();
-                        saveState();
-                    }else{
-                        isClosingCard = true;
-                        window.setTimeout(function () {
-                            card.closeCard()
-                            if(state.matchingCard !== null){
-                                state.matchingCard.closeCard()
-                            }
-                            
-                            increaseMoveCounter();
-
-                            state.isMatching = false;
-                            state.matchingCard = null;
-                            saveState();
-                            isClosingCard = false;
-                        }, 500);
-                    }
-                } else{
-                    state.isMatching = true;
-                    state.matchingCard = card;
-                    saveState();
-                }
-            }, false);
-        });
-    }
-
-    return {
-        init: function(){
-            initGame();
+    function bindClickEvent() {
+     cardDeck.cards.forEach(card => {
+      card.node.addEventListener("click", (e) => {
+       if (isClosingCard || card.isOpened() || card.isMatched()) {
+        return;
+       }
+   
+       card.open();
+   
+       if (state.isMatching) {
+        if (state.matchingCard.getValue() === card.getValue()) {
+   
+         card.match()
+         state.matchingCard.match();
+   
+         if (cardDeck.cards.filter(x => x.isMatched()).length === cardDeck.cards.length) {
+          state.isGameOver = true;
+          saveState();
+          updateScreenMode(true);
+          return;
+         }
+   
+         state.isMatching = false;
+         state.matchingCard = null;
+         increaseMoveCounter();
+         saveState();
+        } else {
+         isClosingCard = true;
+         window.setTimeout(function() {
+          card.closeCard()
+          if (state.matchingCard !== null) {
+           state.matchingCard.closeCard()
+          }
+   
+          increaseMoveCounter();
+   
+          state.isMatching = false;
+          state.matchingCard = null;
+          saveState();
+          isClosingCard = false;
+         }, 500);
         }
+       } else {
+        state.isMatching = true;
+        state.matchingCard = card;
+        saveState();
+       }
+      }, false);
+     });
     }
-}
+   
+    return {
+     init: function() {
+      initGame();
+     }
+    }
+   }
