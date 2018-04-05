7408ae0c4de910ee57b8bc05cc4a419a953a3bae
Homero Barbosa <hbarbosa@paylocity.com>
2018-04-04 19:44:44 -0700

    Rename variables

--- Front-End Web Developer/Project 3/js/entity.js
+++ Front-End Web Developer/Project 3/js/entity.js
@@ -1,6 +1,6 @@
 class Entity {
-    constructor(sprite, x, y, gameSettings) {
-     this.sprite = sprite;
+    constructor(spritePath, x, y, gameSettings) {
+     this.spritePath = spritePath;
      this.currentLocation = {
          x: x,
          y: y
@@ -14,11 +14,11 @@ class Entity {
      // You should multiply any movement by the dt parameter
      // which will ensure the game runs at the same speed for
      // all computers.
-     ctx.drawImage(Resources.get(this.sprite), this.currentLocation.x, this.currentLocation.y, this.gameSettings.player.spriteWidth, this.gameSettings.player.spriteHeight)
+     ctx.drawImage(Resources.get(this.spritePath), this.currentLocation.x, this.currentLocation.y, this.gameSettings.player.spriteWidth, this.gameSettings.player.spriteHeight)
     }
    
     // Draw the enemy on the screen, required method for game
     render(ctx) {
-     ctx.drawImage(Resources.get(this.sprite), this.currentLocation.x, this.currentLocation.y, this.gameSettings.player.spriteWidth, this.gameSettings.player.spriteHeight);
+     ctx.drawImage(Resources.get(this.spritePath), this.currentLocation.x, this.currentLocation.y, this.gameSettings.player.spriteWidth, this.gameSettings.player.spriteHeight);
     }
    }
