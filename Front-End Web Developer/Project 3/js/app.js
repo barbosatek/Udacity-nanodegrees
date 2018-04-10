fe04e077ac976a78d162e39e49aeb14b25f7566c
Homero Barbosa <hbarbosa@paylocity.com>
2018-04-09 17:50:44 -0700

    Clean up app.js

--- Front-End Web Developer/Project 3/js/app.js
+++ Front-End Web Developer/Project 3/js/app.js
@@ -1,68 +1,78 @@
-let background = {
-  spriteWidth: 101,
-  spriteHeight: 171,
-  spriteHeightPadding: 70,
-  spriteWidthPadding: 18,
-  numRows: 6,
-  numCols: 5
-}
+class App {
+  constructor() {
+    const background = {
+      spriteWidth: 101,
+      spriteHeight: 171,
+      spriteHeightPadding: 70,
+      spriteWidthPadding: 18,
+      numRows: 6,
+      numCols: 5
+    }
+    
+    this.gameSettings = {
+       backgroundContext: {
+        spriteWidth: background.spriteWidth,
+        spriteHeight: background.spriteHeight,
+        totalHeight: (background.spriteHeight - background.spriteHeightPadding) * background.numRows,
+        totalWidth: (background.spriteWidth - background.spriteWidthPadding) * background.numCols,
+        spriteHeightPadding: background.spriteHeightPadding,
+        spriteWidthPadding: background.spriteWidthPadding,
+        rowImages: [
+          'images/water-block.png', // Top row is water
+          'images/stone-block.png', // Row 1 of 3 of stone
+          'images/stone-block.png', // Row 2 of 3 of stone
+          'images/stone-block.png', // Row 3 of 3 of stone
+          'images/grass-block.png', // Row 1 of 2 of grass
+          'images/grass-block.png' // Row 2 of 2 of grass
+         ],
+         numRows: background.numRows,
+         numCols: background.numCols
+       },
+       player: {
+        spriteWidth: background.spriteWidth,
+        spriteHeight: background.spriteHeight
+       }
+    }
+    
+    this.playerSprite;
+    this.player;
+    this.allEnemies = [];
 
-const gameSettings = {
-   backgroundContext: {
-    spriteWidth: background.spriteWidth,
-    spriteHeight: background.spriteHeight,
-    totalHeight: (background.spriteHeight - background.spriteHeightPadding) * background.numRows,
-    totalWidth: (background.spriteWidth - background.spriteWidthPadding) * background.numCols,
-    spriteHeightPadding: background.spriteHeightPadding,
-    spriteWidthPadding: background.spriteWidthPadding,
-    rowImages: [
-      'images/water-block.png', // Top row is water
-      'images/stone-block.png', // Row 1 of 3 of stone
-      'images/stone-block.png', // Row 2 of 3 of stone
-      'images/stone-block.png', // Row 3 of 3 of stone
-      'images/grass-block.png', // Row 1 of 2 of grass
-      'images/grass-block.png' // Row 2 of 2 of grass
-     ],
-     numRows: background.numRows,
-     numCols: background.numCols
-   },
-   player: {
-    spriteWidth: 101,
-    spriteHeight: 171
-   }
-}
+    this.setupPlayer(2, 4);
+    this.addEnemy(0,2,3);
+    this.Engine = new Engine(document, window, this.player, this.allEnemies, this.gameSettings);
+    
+    // This listens for key presses and sends the keys to your
+    // Player.handleInput() method. You don't need to modify this.
+    let innerThis = this;
+    document.addEventListener('keyup', function(e) {
+      var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+      };
+    
+      innerThis.player.handleInput(allowedKeys[e.keyCode], innerThis.gameSettings.backgroundContext);
+    });
 
-var getPosition = (col, row) => {
-  let x = gameSettings.backgroundContext.spriteWidth * col;
-  let y = (gameSettings.backgroundContext.spriteHeight - gameSettings.backgroundContext.spriteHeightPadding) * row;
-  
-  return {
-    x,
-    y
+    
   }
-}
 
-let playerPosition = getPosition(1,4);
+  addEnemy(col, row, velocity){
+    let x = this.gameSettings.backgroundContext.spriteWidth * col;
+    let y = (this.gameSettings.backgroundContext.spriteHeight - this.gameSettings.backgroundContext.spriteHeightPadding) * row;
+    let enemySprite = new Sprite('images/enemy-bug.png', this.gameSettings.player.spriteWidth, this.gameSettings.player.spriteHeight, this.gameSettings.backgroundContext.spriteWidthPadding, this.gameSettings.backgroundContext.spriteHeightPadding)
+    this.allEnemies.push(new Enemy(enemySprite, x, y, velocity));
+  }
 
-this.playerSprite = new Sprite('images/char-boy.png', gameSettings.player.spriteWidth, gameSettings.player.spriteHeight, 18, 70)
-this.enemySprite = new Sprite('images/enemy-bug.png', gameSettings.player.spriteWidth, gameSettings.player.spriteHeight, 18, 70)
+  setupPlayer(col, row){
+    let x = this.gameSettings.backgroundContext.spriteWidth * col;
+    let y = (this.gameSettings.backgroundContext.spriteHeight - this.gameSettings.backgroundContext.spriteHeightPadding) * row;
+    
+    let playerSprite = new Sprite('images/char-boy.png', this.gameSettings.player.spriteWidth, this.gameSettings.player.spriteHeight, this.gameSettings.backgroundContext.spriteWidthPadding, this.gameSettings.backgroundContext.spriteHeightPadding)
+    this.player = new Player(playerSprite, x, y);
+  }
+ }
 
-this.allEnemies = [];
-this.allEnemies.push(new Enemy(this.enemySprite, 0, 0, 2));
-this.allEnemies.push(new Enemy(this.enemySprite, 0, gameSettings.backgroundContext.spriteWidth * 2, 3));
-
-this.player = new Player(this.playerSprite, playerPosition.x, playerPosition.y);
-this.Engine = new Engine(document, window, player, allEnemies, gameSettings, gameSettings);
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-  var allowedKeys = {
-    37: 'left',
-    38: 'up',
-    39: 'right',
-    40: 'down'
-  };
-
-  player.handleInput(allowedKeys[e.keyCode], gameSettings.backgroundContext);
-});
+ let app = new App();
