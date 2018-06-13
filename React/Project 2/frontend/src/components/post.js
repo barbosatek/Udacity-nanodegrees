121b939ae156aa1ed82624850ac08adb35e73c48
Homero Barbosa <hbarbosa@paylocity.com>
2018-06-12 19:17:37 -0700

    Finish UI for editing post

--- React/Project 2/frontend/src/components/post.js
+++ React/Project 2/frontend/src/components/post.js
@@ -19,7 +19,9 @@ class Post extends Component {
     const { store } = this.props;
 
     return (
-        <a href="#" data-toggle="collapse" href="#collapseExample" className="list-group-item list-group-item-action flex-column align-items-start">
+        <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+            
+            {/* Title and Vote buttons */}
             <div className="d-flex w-100 justify-content-between">
                 <h5 className="mb-1">{this.state.post.title}</h5>
                 <small>
@@ -30,18 +32,39 @@ class Post extends Component {
                         <button type="button" className="btn btn-light btn-sm">
                             <span className="oi oi-thumb-down"></span>
                         </button>
-                        <button type="button" className="btn btn-link">
-                            <span className="oi oi-pencil"></span>
-                        </button>
                     </div>
                 </small>
             </div>
+
+            {/* Author and VoteScore */}
             <div className="d-flex w-100 justify-content-between">
                 <p>By {this.state.post.author}</p>
                 <small>{this.state.post.voteScore} votes.</small>
             </div>
-            <p className="mb-1">{this.state.post.body}</p>
-            <div className="collapse" id="collapseExample">
+
+            {/* Body and Edit/Delete buttons */}
+            <div className="d-flex w-100 justify-content-between">
+                <p className="mb-1">{this.state.post.body}</p>
+                <small>
+                <div className="btn-group-sm" role="group" aria-label="Basic example">
+                        <button type="button" className="btn btn-link">
+                            <span className="oi oi-pencil"></span>
+                        </button>
+                        <button type="button" className="btn btn-link">
+                            <span class="oi oi-trash"></span>
+                        </button>
+                    </div>
+                </small>
+            </div>
+
+            {/* Author and VoteScore */}
+            <div className="d-flex w-100 justify-content-between">
+                <p>
+                    <a href="#" data-toggle="collapse" href={`#${this.state.post.id}`}>Comments</a>
+                </p>
+            </div>
+            
+            <div className="collapse" id={`${this.state.post.id}`}>
             {Object.keys(store.comments).map((key, index) =>
                 {
                     return store.comments[key].parentId == this.state.post.id && !store.comments[key].deleted && 
