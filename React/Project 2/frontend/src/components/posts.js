00acebf31c88bcf7dab54586550fcf8935ba7b47
Homero Barbosa <hbarbosa@paylocity.com>
2018-06-13 19:01:10 -0700

    Make posts reactive

--- React/Project 2/frontend/src/components/posts.js
+++ React/Project 2/frontend/src/components/posts.js
@@ -2,34 +2,54 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import Post from './post'
 import PostModal from './postModal'
+import * as action from '../actions/index'
 
 class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    }
+
+    this.props.loadPosts()
+  }
+
+
+  componentWillReceiveProps(nextProps) {
+    this.state.posts = nextProps.store.posts;
+  }
+
   render() {
     const { store } = this.props;
 
     return (
       <div className="list-group">
-        {Object.keys(store.posts).map((key, index) =>
-          {
-            return !store.posts[key].deleted && 
+        {this.state.posts && Object.keys(this.state.posts).map((key, index) => {
+          return !this.state.posts[key].deleted &&
             <div>
-              <Post post={store.posts[key]}  key={store.posts[key].id}></Post>
-              <PostModal post={store.posts[key]}  key={store.posts[key].id}></PostModal>
+              <Post post={this.state.posts[key]} key={this.state.posts[key].id}></Post>
+              <PostModal post={this.state.posts[key]} key={this.state.posts[key].id}></PostModal>
             </div>
-          }
+        }
         )}
       </div>
     );
   }
 }
 
+function mapDispatchToProps(dispatch) {
+  return {
+    loadPosts: (data) => dispatch(action.loadPosts())
+  }
+}
+
 function mapStateToProps(store) {
   return {
-      store: {
-        ...store
-      }
+    store: {
+      ...store
     }
+  }
 }
 
 
-export default connect(mapStateToProps)(Posts);
+export default connect(mapStateToProps, mapDispatchToProps)(Posts);
